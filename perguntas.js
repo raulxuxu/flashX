dee9708219cb259e9979f38b2e81a4b8d834c14a
raulxuxu <00001125215100sp@al.educacao.sp.gov.br>
2024-11-26 21:01:37 -0300

    Add files via upload
    eu sou eu

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Curiosidade',
+    'O que é uma for you?',
+    'Do inglês “For You“, a sigla FY do TikTok pode ser traduzida como “Para você” em português.')
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estados Unidos?',
+    'A capital dos Estados Unidos é Washington, D.C.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual maior planeta do sistema solar?',
+    'Além de gigante, o maior planeta do Sistema Solar, Júpiter.'
+)
+
+criaCartao(
+    'Espanhol',
+    'Como se diz oi em Espanhol?',
+    'Comece com o “¡Hola!”. Essa é a saudação básica em espanhol e é utilizada para cumprimentar qualquer pessoa.'
+)
